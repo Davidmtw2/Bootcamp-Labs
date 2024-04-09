a99ad9cc79871476fb2f6fc96bf6b1b5d60ee3bd
Davidmtw2 <davidvir56@gmail.com>
2024-04-09 15:12:22 -0500

    more thoughts

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("This is my leet new app!");
         System.out.println("It will disrupt all industries");
 
+        System.out.println("This is the best app in the universe");
+        System.out.println("It will change everone's lives");
     }
 }
