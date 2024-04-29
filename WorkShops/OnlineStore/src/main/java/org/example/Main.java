e32f4bf7787d57cf9562741c4a79858324ef235f
Davidmtw2 <davidvir56@gmail.com>
2024-04-29 10:47:25 -0500

    changes to the main class

--- WorkShops/OnlineStore/src/main/java/org/example/Main.java
+++ WorkShops/OnlineStore/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Screens screens = new Screens("Thank you for using our Online Store!");
+        Screens screens = new Screens("src/main/resources/products.csv");
         screens.displayMainMenu();
     }
 }
